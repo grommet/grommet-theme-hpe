a679fd917faba1f1ae57b650fb87189ca3b8630c
Eric Soderberg <eric.soderberg@hpe.com>
2019-03-12 12:23:43 -0700

    Fixed an issue with CheckBox checkmark styling

--- src/js/index.js
+++ src/js/index.js
@@ -89,10 +89,6 @@ export const hpe = deepFreeze({
   checkBox: {
     icon: {
       extend: css`
-        box-sizing: border-box;
-        position: absolute;
-        top: 0px;
-        left: 0px;
         width: ${props => props.theme.checkBox.size};
         height: ${props => props.theme.checkBox.size};
       `,
