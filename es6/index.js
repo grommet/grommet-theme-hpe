0c150d022ed54609e7fe6bb311178d4fcd64f9b7
shimisun <shimrit.yacobi@gmail.com>
2020-04-07 13:11:20 -0600

    clean-ups

--- es6/index.js
+++ es6/index.js
@@ -1,17 +1,30 @@
+function _templateObject() {
+  var data = _taggedTemplateLiteralLoose(["\n      ", "\n    "]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+function _taggedTemplateLiteralLoose(strings, raw) { if (!raw) { raw = strings.slice(0); } strings.raw = raw; return strings; }
+
 // (C) Copyright 2020-2021 Hewlett Packard Enterprise Development LP
 import { css } from 'styled-components';
 
-const isObject = item =>
-  item && typeof item === 'object' && !Array.isArray(item);
+var isObject = function isObject(item) {
+  return item && typeof item === 'object' && !Array.isArray(item);
+};
 
-const deepFreeze = obj => {
-  Object.keys(obj).forEach(
-    key => key && isObject(obj[key]) && Object.freeze(obj[key]),
-  );
+var deepFreeze = function deepFreeze(obj) {
+  Object.keys(obj).forEach(function (key) {
+    return key && isObject(obj[key]) && Object.freeze(obj[key]);
+  });
   return Object.freeze(obj);
 };
 
-export const hpe = deepFreeze({
+export var hpe = deepFreeze({
   defaultMode: 'light',
   global: {
     colors: {
@@ -27,45 +40,45 @@ export const hpe = deepFreeze({
       brand: 'green!',
       background: {
         dark: '#1A1F2B',
-        light: '#FFFFFF',
+        light: '#FFFFFF'
       },
       'background-back': {
         dark: '#1A1F2B',
-        light: '#EFEFEF',
+        light: '#EFEFEF'
       },
       'background-front': {
         dark: '#354050',
-        light: '#FFFFFF',
+        light: '#FFFFFF'
       },
       'background-contrast': {
         dark: '#FFFFFF1F',
-        light: '#CCCCCC99',
+        light: '#CCCCCC99'
       },
       icon: 'text',
       text: {
         dark: '#FFFFFF',
-        light: '#333333',
+        light: '#333333'
       },
       'text-strong': {
         dark: '#FFFFFF',
-        light: '#000000',
+        light: '#000000'
       },
       'text-weak': {
         dark: '#CCCCCC',
-        light: '#444444',
+        light: '#444444'
       },
       'text-xweak': {
         dark: '#999999',
-        light: '#666666',
+        light: '#666666'
       },
       border: {
         light: '#CCCCCC',
-        dark: '#444444',
+        dark: '#444444'
       },
       control: 'brand',
       'active-background': {
         dark: '#FFFFFF1F',
-        light: '#CCCCCC99',
+        light: '#CCCCCC99'
       },
       'active-text': 'text-strong',
       'selected-background': 'brand',
@@ -77,37 +90,37 @@ export const hpe = deepFreeze({
       'status-disabled': '#CCCCCC',
       blue: {
         dark: '#0E5265',
-        light: '#00C8FF',
+        light: '#00C8FF'
       },
       'blue!': '#00739D',
       green: {
         dark: '#007A5E',
-        light: '#6FFFB0',
+        light: '#6FFFB0'
       },
       'green!': '#01A982',
       teal: {
         dark: '#007366',
-        light: '#82FFF2',
+        light: '#82FFF2'
       },
       'teal!': '#00E8CF',
       purple: {
         dark: '#371177',
-        light: '#F740FF',
+        light: '#F740FF'
       },
       'purple!': '#7630EA',
       red: {
         dark: '#4B1916',
-        light: '#FF4F4F',
+        light: '#FF4F4F'
       },
       'red!': '#FF0000',
       orange: {
         dark: '#CC4B00',
-        light: '#FFB024',
+        light: '#FFB024'
       },
       'orange!': '#FF8300',
       yellow: {
         dark: '#D78F00',
-        light: '#FFEB59',
+        light: '#FFEB59'
       },
       'yellow!': '#FEC901',
       'graph-0': 'orange!',
@@ -116,177 +129,158 @@ export const hpe = deepFreeze({
       'graph-3': 'yellow!',
       'graph-4': 'teal!',
       focus: 'teal!',
-      placeholder: 'text-weak',
+      placeholder: 'text-weak'
     },
     input: {
-      weight: 500,
+      weight: 500
     },
     font: {
       family: "'Metric', Arial, sans-serif",
-      face: `
-        @font-face {
-          font-family: "Metric";
-          src: url("https://hpefonts.s3.amazonaws.com/web/MetricHPE-Web-Regular.woff") format('woff');
-        }
-        @font-face {
-          font-family: "Metric";
-          src: url("https://hpefonts.s3.amazonaws.com/web/MetricHPE-Web-Bold.woff") format('woff');
-          font-weight: 700;
-        }
-        @font-face {
-          font-family: "Metric";
-          src: url("https://hpefonts.s3.amazonaws.com/web/MetricHPE-Web-Semibold.woff") format('woff');
-          font-weight: 600;
-        }
-        @font-face {
-          font-family: "Metric";
-          src: url("https://hpefonts.s3.amazonaws.com/web/MetricHPE-Web-Light.woff") format('woff');
-          font-weight: 100;
-        }`,
+      face: "\n        @font-face {\n          font-family: \"Metric\";\n          src: url(\"https://hpefonts.s3.amazonaws.com/web/MetricHPE-Web-Regular.woff\") format('woff');\n        }\n        @font-face {\n          font-family: \"Metric\";\n          src: url(\"https://hpefonts.s3.amazonaws.com/web/MetricHPE-Web-Bold.woff\") format('woff');\n          font-weight: 700;\n        }\n        @font-face {\n          font-family: \"Metric\";\n          src: url(\"https://hpefonts.s3.amazonaws.com/web/MetricHPE-Web-Semibold.woff\") format('woff');\n          font-weight: 600;\n        }\n        @font-face {\n          font-family: \"Metric\";\n          src: url(\"https://hpefonts.s3.amazonaws.com/web/MetricHPE-Web-Light.woff\") format('woff');\n          font-weight: 100;\n        }"
     },
     active: {
       background: 'active-background',
-      color: 'active-text',
+      color: 'active-text'
     },
     drop: {
       background: 'background-front',
       border: {
-        radius: '8px',
+        radius: '8px'
       },
-      shadowSize: 'medium',
+      shadowSize: 'medium'
     },
     elevation: {
       dark: {
-        medium: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
-      },
+        medium: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)'
+      }
     },
     hover: {
       background: 'active-background',
-      color: 'active-text',
+      color: 'active-text'
     },
     selected: {
       background: 'selected-background',
-      color: 'selected-text',
-    },
+      color: 'selected-text'
+    }
   },
   accordion: {
     border: {
-      color: 'text',
+      color: 'text'
     },
     icons: {
-      color: 'text',
-    },
+      color: 'text'
+    }
   },
   anchor: {
     color: 'text',
     textDecoration: 'underline',
     fontWeight: 500,
     hover: {
-      textDecoration: 'underline',
-    },
+      textDecoration: 'underline'
+    }
   },
   button: {
     size: {
       small: {
         border: {
-          radius: '4px',
+          radius: '4px'
         },
         pad: {
           vertical: '4px',
-          horizontal: '8px',
-        },
+          horizontal: '8px'
+        }
       },
       medium: {
         border: {
-          radius: '4px',
+          radius: '4px'
         },
         pad: {
           vertical: '4px',
-          horizontal: '10px',
-        },
+          horizontal: '10px'
+        }
       },
       large: {
         border: {
-          radius: '6px',
+          radius: '6px'
         },
         pad: {
           vertical: '6px',
-          horizontal: '16px',
-        },
-      },
+          horizontal: '16px'
+        }
+      }
     },
     border: {
-      radius: '4px',
+      radius: '4px'
     },
     padding: {
       vertical: '4px',
-      horizontal: '10px',
+      horizontal: '10px'
     },
-    extend: css`
-      ${props => !props.plain && 'font-weight: bold;'}
-    `,
+    extend: css(_templateObject(), function (props) {
+      return !props.plain && 'font-weight: bold;';
+    })
   },
   calendar: {
     small: {
       fontSize: '13.6px',
       lineHeight: 1.375,
-      daySize: '27.43px',
+      daySize: '27.43px'
     },
     medium: {
       fontSize: '18px',
       lineHeight: 1.45,
-      daySize: '54.86px',
+      daySize: '54.86px'
     },
     large: {
       fontSize: '31.2px',
       lineHeight: 1.11,
-      daySize: '109.71px',
-    },
+      daySize: '109.71px'
+    }
   },
   checkBox: {
     color: 'selected-text',
     gap: 'small',
     check: {
-      radius: '2px',
+      radius: '2px'
     },
     border: {
-      width: '1px',
-    },
+      width: '1px'
+    }
   },
   formField: {
     border: {
-      side: 'all',
+      side: 'all'
     },
     error: {
       size: 'xsmall',
       color: 'text-xweak',
       margin: {
-        start: 'none',
-      },
+        start: 'none'
+      }
     },
     help: {
       size: 'xsmall',
       color: 'text-xweak',
       margin: {
         start: 'none',
-        bottom: 'xsmall',
-      },
+        bottom: 'xsmall'
+      }
     },
     info: {
       size: 'xsmall',
       color: 'text-xweak',
       margin: {
-        start: 'none',
-      },
+        start: 'none'
+      }
     },
     label: {
       size: 'xsmall',
       color: 'text-weak',
       margin: {
-        horizontal: 'none',
-      },
+        horizontal: 'none'
+      }
     },
-    round: '4px',
+    round: '4px'
   },
   heading: {
     level: {
@@ -294,246 +288,246 @@ export const hpe = deepFreeze({
         small: {
           size: '36px',
           height: '42px',
-          maxWidth: '854px',
+          maxWidth: '854px'
         },
         medium: {
           size: '53px',
           height: '59px',
-          maxWidth: '1277px',
+          maxWidth: '1277px'
         },
         large: {
           size: '88px',
           height: '94px',
-          maxWidth: '2122px',
+          maxWidth: '2122px'
         },
         xlarge: {
           size: '124px',
           height: '130px',
-          maxWidth: '2966px',
-        },
+          maxWidth: '2966px'
+        }
       },
       '2': {
         small: {
           size: '31px',
           height: '37px',
-          maxWidth: '749px',
+          maxWidth: '749px'
         },
         medium: {
           size: '44px',
           height: '50px',
-          maxWidth: '1066px',
+          maxWidth: '1066px'
         },
         large: {
           size: '58px',
           height: '64px',
-          maxWidth: '1382px',
+          maxWidth: '1382px'
         },
         xlarge: {
           size: '71px',
           height: '77px',
-          maxWidth: '1699px',
-        },
+          maxWidth: '1699px'
+        }
       },
       '3': {
         small: {
           size: '27px',
           height: '33px',
-          maxWidth: '643px',
+          maxWidth: '643px'
         },
         medium: {
           size: '36px',
           height: '42px',
-          maxWidth: '854px',
+          maxWidth: '854px'
         },
         large: {
           size: '44px',
           height: '50px',
-          maxWidth: '1066px',
+          maxWidth: '1066px'
         },
         xlarge: {
           size: '53px',
           height: '59px',
-          maxWidth: '1277px',
-        },
+          maxWidth: '1277px'
+        }
       },
       '4': {
         small: {
           size: '22px',
           height: '28px',
-          maxWidth: '538px',
+          maxWidth: '538px'
         },
         medium: {
           size: '27px',
           height: '33px',
-          maxWidth: '643px',
+          maxWidth: '643px'
         },
         large: {
           size: '31px',
           height: '37px',
-          maxWidth: '749px',
+          maxWidth: '749px'
         },
         xlarge: {
           size: '36px',
           height: '42px',
-          maxWidth: '854px',
-        },
+          maxWidth: '854px'
+        }
       },
       '5': {
         small: {
           size: '16px',
           height: '22px',
-          maxWidth: '379px',
+          maxWidth: '379px'
         },
         medium: {
           size: '16px',
           height: '22px',
-          maxWidth: '379px',
+          maxWidth: '379px'
         },
         large: {
           size: '16px',
           height: '22px',
-          maxWidth: '379px',
+          maxWidth: '379px'
         },
         xlarge: {
           size: '16px',
           height: '22px',
-          maxWidth: '379px',
-        },
+          maxWidth: '379px'
+        }
       },
       '6': {
         small: {
           size: '14px',
           height: '20px',
-          maxWidth: '326px',
+          maxWidth: '326px'
         },
         medium: {
           size: '14px',
           height: '20px',
-          maxWidth: '326px',
+          maxWidth: '326px'
         },
         large: {
           size: '14px',
           height: '20px',
-          maxWidth: '326px',
+          maxWidth: '326px'
         },
         xlarge: {
           size: '14px',
           height: '20px',
-          maxWidth: '326px',
-        },
-      },
+          maxWidth: '326px'
+        }
+      }
     },
-    weight: 700,
+    weight: 700
   },
   icon: {
     size: {
-      xxlarge: '166px',
-    },
+      xxlarge: '166px'
+    }
   },
   layer: {
-    background: 'background',
+    background: 'background'
   },
   paragraph: {
     small: {
       size: '16px',
       height: '22px',
-      maxWidth: '379px',
+      maxWidth: '379px'
     },
     medium: {
       size: '18px',
       height: '24px',
-      maxWidth: '432px',
+      maxWidth: '432px'
     },
     large: {
       size: '22px',
       height: '28px',
-      maxWidth: '538px',
+      maxWidth: '538px'
     },
     xlarge: {
       size: '27px',
       height: '33px',
-      maxWidth: '643px',
+      maxWidth: '643px'
     },
     xxlarge: {
       size: '36px',
       height: '42px',
-      maxWidth: '854px',
-    },
+      maxWidth: '854px'
+    }
   },
   radioButton: {
     color: 'text-strong',
     check: {
-      color: 'text-strong',
+      color: 'text-strong'
     },
-    gap: 'medium',
+    gap: 'medium'
   },
   rangeInput: {
     track: {
-      color: 'background-contrast',
+      color: 'background-contrast'
     },
     thumb: {
-      color: 'text',
-    },
+      color: 'text'
+    }
   },
   select: {
     icons: {
-      color: 'text',
+      color: 'text'
     },
     options: {
       text: {
-        size: 'small',
-      },
-    },
+        size: 'small'
+      }
+    }
   },
   tab: {
     border: {
-      color: 'text-xweak',
+      color: 'text-xweak'
     },
     color: 'text-xweak',
     margin: {
-      horizontal: 'none',
+      horizontal: 'none'
     },
     pad: {
-      horizontal: 'small',
-    },
+      horizontal: 'small'
+    }
   },
   text: {
     xsmall: {
       size: '14px',
       height: '20px',
-      maxWidth: '326px',
+      maxWidth: '326px'
     },
     small: {
       size: '16px',
       height: '22px',
-      maxWidth: '379px',
+      maxWidth: '379px'
     },
     medium: {
       size: '18px',
       height: '24px',
-      maxWidth: '432px',
+      maxWidth: '432px'
     },
     large: {
       size: '22px',
       height: '28px',
-      maxWidth: '538px',
+      maxWidth: '538px'
     },
     xlarge: {
       size: '27px',
       height: '33px',
-      maxWidth: '643px',
+      maxWidth: '643px'
     },
     xxlarge: {
       size: '36px',
       height: '42px',
-      maxWidth: '854px',
-    },
+      maxWidth: '854px'
+    }
   },
   // Theme-Designer only parameters
   name: 'HPE 1',
   rounding: 4,
   scale: 1.1,
-  spacing: 24,
+  spacing: 24
 });
-
-export const { colors } = hpe.global;
+var colors = hpe.global.colors;
+export { colors };
