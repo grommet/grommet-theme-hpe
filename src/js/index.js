f401e9e6dade349035030aa511ec1a6099cf95cd
taysea <taylor.seamans@yahoo.com>
2020-04-21 13:31:31 -0700

    Allow input to respect font line-height (#48)

--- src/js/index.js
+++ src/js/index.js
@@ -119,11 +119,14 @@ export const hpe = deepFreeze({
       placeholder: 'disabled-text',
     },
     input: {
+      font: {
+        height: 'inherit',
+        weight: 500,
+      },
       padding: {
         horizontal: 'small',
         vertical: 'xsmall',
       },
-      weight: 500,
     },
     font: {
       family: "'Metric', Arial, sans-serif",
