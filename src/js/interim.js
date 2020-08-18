c8c6c0de87dcadd135cd4ef7fa79ef9680f3cbcc
shimisun <shimrit.yacobi@gmail.com>
2020-08-17 19:32:16 -0600

    Adding descriptive action-items per colors

--- src/js/interim.js
+++ src/js/interim.js
@@ -1,6 +1,7 @@
 /**
  * The following theme has transitional properties to
- * mitigate the upgrade to the design-system theme.
+ * mitigate the upgrade to the design-system theme, i.e. migrating from hpe
+ * theme version ^1.0.0 to ^2.0.0
  * Using the interim theme will help you avoid critical
  * and immediate breaking changes.
  *
@@ -11,14 +12,18 @@
  * Copying the content of this file will help you avoid immediate
  * breaking changes behavior.
  * However the main goal is for your theme to work independently without
- * the interim file support.
+ * the interim theme support.
  *
  */
 
 export const interimTheme = {
   global: {
     colors: {
+      // accent-1 used to be the focus color, use 'green' color instead.
       'accent-1': '#6FFFB0',
+      // All accent and neutral colors should be replaced with 'graph-*' colors,
+      // or with the branded HPE colors from the design-system
+      // theme (grommet-theme-hpe v2).
       'accent-2': '#FD6FFF',
       'accent-3': '#81FCED',
       'accent-4': '#FFCA58',
@@ -26,6 +31,11 @@ export const interimTheme = {
       'neutral-2': '#3D138D',
       'neutral-3': '#00739D',
       'neutral-4': '#A2423D',
+      // Any references to status-error, should change to status-critical.
+      'status-error': {
+        dark: '#FC5A5A',
+        light: '#D04F4E',
+      },
     },
   },
 };
