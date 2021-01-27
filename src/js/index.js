3089dc6aa9fc23ddff0fa56298392d60569dae2d
taysea <taylor.seamans@yahoo.com>
2021-01-27 11:37:45 -0800

    Set default card elevation and fix hex value (#157)

--- src/js/index.js
+++ src/js/index.js
@@ -228,8 +228,8 @@ export const hpe = deepFreeze({
       // As defined here, default is currently mapping to medium.
       light: {
         small: '0px 2px 4px #0000001F;',
-        medium: '0px 6px 12px #0000003D;',
-        large: '0px 12px 24px #0000001F;',
+        medium: '0px 6px 12px #0000001F;',
+        large: '0px 12px 24px #0000003D;',
       },
       dark: {
         small: '0px 2px 4px #0000003D;',
@@ -450,6 +450,7 @@ export const hpe = deepFreeze({
   card: {
     container: {
       background: 'background-front',
+      elevation: 'medium',
     },
     body: {
       pad: 'medium',
