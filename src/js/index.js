47a42ec2f93849a86d6e6b582630ee77699a6a28
britt6612 <42451602+britt6612@users.noreply.github.com>
2021-03-19 13:28:38 -0600

    add theme for spinner (#176)
    Co-authored-by: Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>

--- src/js/index.js
+++ src/js/index.js
@@ -1003,6 +1003,16 @@ export const hpe = deepFreeze({
     },
     options: undefined,
   },
+  spinner: {
+    container: {
+      border: [
+        { side: 'all', color: 'background-contrast', size: 'medium' },
+        { side: 'right', color: 'green', size: 'medium' },
+        { side: 'top', color: 'green', size: 'medium' },
+        { side: 'left', color: 'green', size: 'medium' },
+      ],
+    },
+  },
   tab: {
     color: 'text-strong',
     active: {
