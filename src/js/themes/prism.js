bdd5d48aa6443354a4e1ca7ea586bbbadc1c689c
Jeff Stolz <jstolz123@gmail.com>
2021-10-26 16:16:14 -0400

    Update colors for code block comments (#219)
    Co-authored-by: Jeffrey Stolz <jeffstolz@Jeffreys-MacBook-Pro.local>

--- src/js/themes/prism.js
+++ src/js/themes/prism.js
@@ -23,7 +23,7 @@ const colors = {
   },
   char: { dark: hpeColors['teal!'], light: hpeColors.teal.dark },
   code: hpeColors.text,
-  comment: hpeColors['status-unknown'],
+  comment: { dark: hpeColors.green.light, light: hpeColors.green.dark },
   entity: { dark: hpeColors.blue.light, light: hpeColors.blue.dark },
   function: {
     dark: hpeColors['status-critical'].light,
