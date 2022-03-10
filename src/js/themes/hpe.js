d7819cd4dfe6afa175ed03536ccf8f74d98658a7
Jessica Filben <jessica.filben@gmail.com>
2022-03-10 13:10:11 -0700

    added trailing comma

--- src/js/themes/hpe.js
+++ src/js/themes/hpe.js
@@ -851,7 +851,7 @@ export const hpe = deepFreeze({
         min: '336px', // 336 + 24 (margin) + 24 (margin) = 384 (e.g. 'medium')
         max: '100%',
       },
-    }
+    },
   },
   pagination: {
     button: {
