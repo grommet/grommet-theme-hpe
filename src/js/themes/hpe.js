6571ed63b95bc35ceb4caa156a9d799ee0c56b09
britt6612 <42451602+britt6612@users.noreply.github.com>
2022-04-04 15:20:11 -0600

    add breakpointts from aries theme and pad for page (#246)
    * add breakpointts from aries theme and pad for page
    
    * add back full
    
    * add min and max

--- src/js/themes/hpe.js
+++ src/js/themes/hpe.js
@@ -13,6 +13,8 @@ import { Unsorted } from 'grommet-icons/icons/Unsorted';
 
 import { colors } from './colors';
 
+const baseSpacing = 24;
+
 const isObject = (item) =>
   item && typeof item === 'object' && !Array.isArray(item);
 
@@ -26,6 +28,47 @@ const deepFreeze = (obj) => {
 export const hpe = deepFreeze({
   defaultMode: 'light',
   global: {
+    breakpoints: {
+      xsmall: {
+        value: baseSpacing * 24, // 576
+        borderSize: {
+          xsmall: '1px',
+          small: '2px',
+          medium: `${baseSpacing / 6}px`, // 4
+          large: `${baseSpacing / 4}px`, // 6
+          xlarge: `${baseSpacing / 2}px`, // 12
+        },
+        edgeSize: {
+          none: '0px',
+          hair: '1px', // for Chart
+          xxsmall: '2px',
+          xsmall: `${baseSpacing / 8}px`, // 3
+          small: `${baseSpacing / 4}px`, // 6
+          medium: `${baseSpacing / 2}px`, // 12
+          large: `${baseSpacing}px`, // 24
+          xlarge: `${baseSpacing * 2}px`, // 48
+        },
+        size: {
+          xxsmall: `${baseSpacing}px`, // 24
+          xsmall: `${baseSpacing * 2}px`, // 48
+          small: `${baseSpacing * 4}px`, // 96
+          medium: `${baseSpacing * 8}px`, // 192
+          large: `${baseSpacing * 16}px`, // 384
+          xlarge: `${baseSpacing * 32}px`, // 768
+          full: '100%',
+        },
+      },
+      small: {
+        value: baseSpacing * 32, // 768
+      },
+      medium: {
+        value: baseSpacing * 45, // 1080
+      },
+      large: {
+        value: baseSpacing * 60, // 1440
+      },
+      xlarge: {}, // anything larger than 1440,
+    },
     colors,
     input: {
       font: {
@@ -906,17 +949,35 @@ export const hpe = deepFreeze({
         min: '336px', // 336 + 24 (margin) + 24 (margin) = 384 (e.g. 'medium')
         max: 'xxlarge',
       },
+      xsmall: {
+        pad: { horizontal: 'large' },
+      },
+      xlarge: {
+        pad: { horizontal: 'large' },
+      },
     },
     narrow: {
       width: {
         min: '336px', // 336 + 24 (margin) + 24 (margin) = 384 (e.g. 'medium')
         max: 'large',
       },
+      xsmall: {
+        pad: { horizontal: 'large' },
+      },
+      xlarge: {
+        pad: { horizontal: 'large' },
+      },
     },
     full: {
       width: {
         min: '336px', // 336 + 24 (margin) + 24 (margin) = 384 (e.g. 'medium')
         max: '100%',
+      },
+      xsmall: {
+        pad: { horizontal: 'large' },
+      },
+      xlarge: {
+        pad: { horizontal: 'large' },
       },
     },
   },
