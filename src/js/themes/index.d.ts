75ed4c5270259f3d25966bd5b00d126bd1d34b2c
Matt Glissmann <mdglissmann@gmail.com>
2023-01-27 16:45:58 -0700

    update with latest on-brand specs

--- src/js/themes/index.d.ts
+++ src/js/themes/index.d.ts
@@ -1,6 +1,7 @@
 declare const hpe: object;
+declare const hpeWeb: object;
 declare const backgrounds: object;
 declare const colors: object;
 declare const prism: object;
 
-export { hpe, backgrounds, colors, prism };
+export { hpe, hpeWeb, backgrounds, colors, prism };
