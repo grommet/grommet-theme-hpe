592931c009a094e3189e0cfc4ba1d28993c644e1
Matt Glissmann <mdglissmann@gmail.com>
2023-03-01 09:35:28 -0700

    initial audit theme and rules

---
+++ src/js/utils/deepMerge.js
@@ -0,0 +1,28 @@
+export const isObject = (item) =>
+  item && typeof item === 'object' && !Array.isArray(item);
+
+export const deepMerge = (target, ...sources) => {
+  if (!sources.length) {
+    return target;
+  }
+
+  const output = { ...target };
+
+  sources.forEach((source) => {
+    if (isObject(source)) {
+      Object.keys(source).forEach((key) => {
+        if (isObject(source[key])) {
+          if (!output[key]) {
+            output[key] = { ...source[key] };
+          } else {
+            output[key] = deepMerge(output[key], source[key]);
+          }
+        } else {
+          output[key] = source[key];
+        }
+      });
+    }
+  });
+
+  return output;
+};
