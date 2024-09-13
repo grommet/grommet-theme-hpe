a2bb8b316a35d8fc0fb585ccbcb0818a38708932
taysea <taylor.seamans@yahoo.com>
2024-09-12 17:07:45 -0700

    Add Github Action to update design-tokens-stable

--- tools/release-stable.js
+++ tools/release-stable.js
@@ -7,18 +7,28 @@ import { simpleGit as git } from 'simple-git';
 
 const repoURL = `https://${process.env.GH_TOKEN}@github.com/grommet/grommet-theme-hpe.git`;
 const localFolder = path.resolve('.tmp/grommet-theme-hpe');
-const localDist = path.resolve('dist');
+const BUILD_DIR = 'dist';
+const localDist = path.resolve(BUILD_DIR);
+
+const BRANCH = 'design-tokens-stable';
+
+const files = ['package.json', 'README.md', 'LICENSE'];
 
 if (process.env.CI) {
   del(localFolder).then(() => {
     git()
       .clone(repoURL, localFolder)
-      .then(() => git(localFolder).checkout('stable'))
+      .then(() => git(localFolder).checkout(BRANCH))
       .then(() => del([`${localFolder}/**/*`]))
-      .then(() => fs.copy(localDist, localFolder))
+      .then(() => fs.copy(localDist, `${localFolder}/${BUILD_DIR}`))
+      .then(() =>
+        files.forEach((file) =>
+          fs.copyFile(path.resolve(file), `${localFolder}/${file}`),
+        ),
+      )
       .then(() => git(localFolder).add(['--all', '.']))
-      .then(() => git(localFolder).commit('stable updated'))
-      .then(() => git(localFolder).push('origin', 'stable'))
+      .then(() => git(localFolder).commit(`${BRANCH} updated`))
+      .then(() => git(localFolder).push('origin', BRANCH))
       .catch((err) => console.error('failed: ', err));
   });
 } else {
