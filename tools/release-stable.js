a7e9b09861d82fdfee0e7f65c85e2aa300e00102
Taylor Seamans <taylor.seamans@yahoo.com>
2025-03-20 14:26:16 -0700

    Ensure stable branches have package.json (#454)

--- tools/release-stable.js
+++ tools/release-stable.js
@@ -7,18 +7,30 @@ import { simpleGit as git } from 'simple-git';
 
 const repoURL = `https://${process.env.GH_TOKEN}@github.com/grommet/grommet-theme-hpe.git`;
 const localFolder = path.resolve('.tmp/grommet-theme-hpe');
-const localDist = path.resolve('dist');
+const BUILD_DIR = 'dist';
+const localDist = path.resolve(BUILD_DIR);
+
+const [BRANCH] = process.argv
+  .find((v) => v.includes('--branch='))
+  .split('=')
+  .slice(-1);
 
 if (process.env.CI) {
   del(localFolder).then(() => {
     git()
       .clone(repoURL, localFolder)
-      .then(() => git(localFolder).checkout('stable'))
+      .then(() => git(localFolder).checkout(BRANCH))
       .then(() => del([`${localFolder}/**/*`]))
-      .then(() => fs.copy(localDist, localFolder))
+      .then(() => fs.copy(localDist, `${localFolder}/${BUILD_DIR}`))
+      .then(() =>
+        fs.copyFile(
+          path.resolve('package.json'),
+          `${localFolder}/${'package.json'}`,
+        ),
+      )
       .then(() => git(localFolder).add(['--all', '.']))
-      .then(() => git(localFolder).commit('stable updated'))
-      .then(() => git(localFolder).push('origin', 'stable'))
+      .then(() => git(localFolder).commit(`${BRANCH} updated`))
+      .then(() => git(localFolder).push('origin', BRANCH))
       .catch((err) => console.error('failed: ', err));
   });
 } else {
