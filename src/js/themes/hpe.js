1f847707fc46836c583a805cd9ca7ca5766001b2
taysea <taylor.seamans@yahoo.com>
2025-05-07 10:08:01 -0700

    Adjust tip.content.margin to xsmall

--- src/js/themes/hpe.js
+++ src/js/themes/hpe.js
@@ -2441,7 +2441,7 @@ const buildTheme = (tokens, flags) => {
       content: {
         background: 'background-floating',
         border: { color: 'border-weak' },
-        margin: 'xxsmall',
+        margin: 'xsmall',
         elevation: 'small',
         pad: { vertical: 'none', horizontal: 'small' },
         round: components.hpe.drop.default.borderRadius,
