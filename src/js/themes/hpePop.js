3c202b197db8efab9718969661fe7e3794e08be4
Matthew Glissmann <mdglissmann@gmail.com>
2025-05-20 15:07:06 -0600

    Updated marketing typography scales

--- src/js/themes/hpePop.js
+++ src/js/themes/hpePop.js
@@ -32,202 +32,194 @@ export const deepMerge = (target, ...sources) => {
 export const hpePop = deepMerge(hpe, {
   heading: {
     color: 'text-strong',
-    weight: 400,
+    weight: 500,
     level: {
       1: {
-        font: { weight: 400 },
         small: {
           size: '3rem', // 48px
           height: '3rem', // 48px
         },
         medium: {
-          size: '4.5rem', // 72px
-          height: '4.5rem', // 72px
+          size: '4.25rem', // 68px
+          height: '4.625rem', // 74px
         },
         large: {
+          size: '5.25rem', // 84px
+          height: '5.625rem', // 90px
+        },
+        xlarge: {  // Heading condensed
           size: '6rem', // 96px
           height: '6rem', // 96px
         },
-        xlarge: {
-          size: '7.5rem', // 120px
-          height: '7.5rem', // 120px
-        },
+        xxlarge: { // Heading large condensed
+          size: '7rem', // 112px
+          height: '7rem', // 112px
+        }
       },
       2: {
         small: {
           size: '2.25rem', // 36px
-          height: '2.25rem', // 36px
+          height: '2.625rem', // 42px
         },
         medium: {
-          size: '3rem', // 48px
-          height: '3rem', // 48px
+          size: '3.25rem', // 52px
+          height: '3.625rem', // 58px
         },
         large: {
-          size: '4.5rem', // 72px
-          height: '4.5rem', // 72px
+          size: '4.25rem', // 68px
+          height: '4.625rem', // 74px
         },
-        xlarge: {
+        xlarge: { // Heading condensed
+          size: '5.25rem', // 84px
+          height: '5.625rem', // 90px
+        },
+        xxlarge: { // Heading large condensed
           size: '6rem', // 96px
           height: '6rem', // 96px
-        },
+        }
       },
       3: {
         small: {
-          size: '1.5rem', // 24px
-          height: '1.5rem', // 24px
+   
         },
         medium: {
           size: '2.25rem', // 36px
-          height: '2.25rem', // 36px
+          height: '2.625rem', // 42px
         },
         large: {
-          size: '3rem', // 48px
-          height: '3rem', // 48px
+          size: '3.25rem', // 52px
+          height: '3.625rem', // 58px
         },
-        xlarge: {
-          size: '4.5rem', // 72px
-          height: '4.5rem', // 72px
+        xlarge: { // Heading condensed
+          size: '4.25rem', // 68px
+          height: '4.625rem', // 74px
         },
+        xxlarge: { // Heading large condensed
+          size: '5.25rem', // 84px
+          height: '5.625rem', // 90px
+        }
       },
       4: {
-        font: { weight: 500 },
         small: {
-          size: '1.125rem', // 18px
-          height: '1.125rem', // 18px
+          size: '1.125rem', // 20px
+          height: '1.125rem', // 26px
         },
         medium: {
-          size: '1.5rem', // 24px
-          height: '1.5rem', // 24px
+          size: '1.5rem', // 28px
+          height: '1.5rem', // 34px
         },
         large: {
           size: '2.25rem', // 36px
-          height: '2.25rem', // 36px
+          height: '2.625rem', // 42px
         },
         xlarge: {
-          size: '3rem', // 48px
-          height: '3rem', // 48px
-        },
+          size: '3.25rem', // 52px
+          height: '3.625rem', // 58px
+        }
       },
       5: {
-        font: { weight: 500 },
         small: {
           size: '1rem', // 16px
-          height: '1rem', // 16px
+          height: '1.375rem', // 22px
         },
         medium: {
-          size: '1.125rem', // 18px
-          height: '1.125rem', // 18px
+          size: '1.125rem', // 20px
+          height: '1.125rem', // 26px
         },
         large: {
-          size: '1.5rem', // 24px
-          height: '1.5rem', // 24px
+          size: '1.5rem', // 28px
+          height: '1.5rem', // 34px
         },
         xlarge: {
           size: '2.25rem', // 36px
-          height: '2.25rem', // 36px
+          height: '2.625rem', // 42px
         },
       },
       6: {
-        font: { weight: 500 },
         small: {
-          size: '0.875rem', // 14px
-          height: '0.875rem', // 14px
+          size: '1rem', // 16px
+          height: '1.375rem', // 22px
         },
         medium: {
           size: '1rem', // 16px
-          height: '1rem', // 16px
+          height: '1.375rem', // 22px
         },
         large: {
-          size: '1.125rem', // 18px
-          height: '1.125rem', // 18px
+          size: '1.125rem', // 20px
+          height: '1.125rem', // 26px
         },
         xlarge: {
-          size: '1.5rem', // 24px
-          height: '1.5rem', // 24px
+          size: '2.25rem', // 36px
+          height: '2.625rem', // 42px
         },
       },
     },
     extend: ({ level, size }) => {
-      let fontWeight = '';
-      if (level === 3 && size === 'small') {
-        fontWeight = 'font-weight: 500;';
-      } else if (level === 4 && ['large', 'xlarge'].includes(size)) {
-        fontWeight = 'font-weight: 400;';
-      } else if (level === 5 && size === 'xlarge') {
-        fontWeight = 'font-weight: 400;';
-      } else if (level === 6 && size === 'small') {
-        fontWeight = 'font-weight: 600;';
+      let font = '';
+      if ([1, 2, 3].includes(level) && ['xlarge', 'xxlarge'].includes(size)) {
+        font = 'font-weight: 700; font-family: GraphikXXCondensed;';
       }
-      return fontWeight;
+      return font;
     },
   },
   paragraph: {
-    small: {
-      size: '1rem', // 16px
+    xsmall: { // disclaimer
+      size: '0.875rem', // 14px
       height: '1.25rem', // 20px
     },
-    medium: {
-      size: '1.125rem', // 18px
-      height: '1.375rem', // 22px
+    small: { // small body
+      size: '1rem', // 16px
+      height: '1.5rem', // 24px
     },
-    large: {
-      size: '1.5rem', // 24px
+    medium: { // body
+      size: '1.25rem', // 20px
       height: '1.875rem', // 30px
     },
-    xlarge: {
+    large: { // large body
+      size: '1.75rem', // 28px
+      height: '2.375rem', // 38px
+    },
+    xlarge: { // small quote
       size: '2.25rem', // 36px
-      height: '2.625rem', // 42px
+      height: '2.875rem', // 46px
     },
-    xxlarge: {
-      size: '2.625rem', // 42px
-      height: '3rem', // 48px
+    xxlarge: { // quote
+      size: '2.75rem', // 44px
+      height: '3.375rem', // 54px
     },
-    extend: ({ size }) => {
-      if (['large', 'xlarge', 'xxlarge'].includes(size))
-        return 'font-weight: 300;';
-      return '';
+    xxxlarge: { // large quote
+      size: '3.25rem', // 52px
+      height: '3.875rem', // 62px
     },
   },
   text: {
-    xsmall: {
-      // weight needs to be modified at the size level
+    xsmall: { // disclaimer
       size: '0.875rem', // 14px
-      height: '1.125rem', // 18px
-    },
-    small: {
-      // weight needs to be modified at the size level
-      size: '1rem', // 16px
       height: '1.25rem', // 20px
     },
-    medium: {
-      // weight needs to be modified at the size level
-      size: '1.125rem', // 18px
-      height: '1.375rem', // 22px
+    small: { // small body
+      size: '1rem', // 16px
+      height: '1.5rem', // 24px
     },
-    large: {
-      // weight needs to be modified at the size level by bumping down to 300
-      size: '1.5rem', // 24px
+    medium: { // body
+      size: '1.25rem', // 20px
       height: '1.875rem', // 30px
     },
-    xlarge: {
-      // weight needs to be modified at the size level by bumping down to 300
+    large: { // large body
+      size: '1.75rem', // 28px
+      height: '2.375rem', // 38px
+    },
+    xlarge: { // small quote
       size: '2.25rem', // 36px
-      height: '2.625rem', // 42px
+      height: '2.875rem', // 46px
     },
-    // xxlarge is not part of Chris's type exploration
-    xxlarge: {
-      // weight needs to be modified at the size level by bumping down to 300
-      size: '2.625rem', // 42px
-      height: '3rem', // 48px
+    xxlarge: { // quote
+      size: '2.75rem', // 44px
+      height: '3.375rem', // 54px
     },
-    extend: ({ size }) => {
-      if (
-        ['large', 'xlarge', 'xxlarge', '3xl', '4xl', '5xl', '6xl'].includes(
-          size,
-        )
-      )
-        return 'font-weight: 300;';
-      return '';
+    xxxlarge: { // large quote
+      size: '3.25rem', // 52px
+      height: '3.875rem', // 62px
     },
   },
 });
