d8394655ec7a120c21de3e9aa1c51b5ff6e0919a
taysea <taylor.seamans@yahoo.com>
2025-05-27 16:18:38 -0700

    Remove underline on anchor sizes

--- src/js/themes/hpePop.js
+++ src/js/themes/hpePop.js
@@ -91,6 +91,38 @@ const popButtonSizes = {
 export const hpePop = deepMerge(hpe, {
   anchor: {
     textDecoration: 'none',
+    size: {
+      xsmall: {
+        textDecoration: 'none',
+      },
+      small: {
+        textDecoration: 'none',
+      },
+      medium: {
+        textDecoration: 'none',
+      },
+      large: {
+        textDecoration: 'none',
+      },
+      xlarge: {
+        textDecoration: 'none',
+      },
+      xxlarge: {
+        textDecoration: 'none',
+      },
+      '3xl': {
+        textDecoration: 'none',
+      },
+      '4xl': {
+        textDecoration: 'none',
+      },
+      '5xl': {
+        textDecoration: 'none',
+      },
+      '6xl': {
+        textDecoration: 'none',
+      },
+    },
   },
   button: {
     secondary: {
