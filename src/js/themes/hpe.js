506db4a7d693bfa2cdd3cc66b77580fe21943bd3
taysea <taylor.seamans@yahoo.com>
2025-07-21 16:34:37 -0700

    Introduce theme-defined bold

--- src/js/themes/hpe.js
+++ src/js/themes/hpe.js
@@ -379,11 +379,17 @@ const buildTheme = (tokens, flags) => {
       large.hpe.text?.[textSize]?.fontWeight || fallback.weight;
   });
 
-  textTheme.extend = ({ size: textSize, weight }) =>
-    !weight ? `font-weight: ${fontWeights[textSize]};` : '';
+  textTheme.extend = ({ size: textSize, weight }) => {
+    if (!weight) return `font-weight: ${fontWeights[textSize]};`;
+    if (weight === 'bold') return `font-weight: 500;`;
+    return '';
+  };
 
-  paragraphTheme.extend = ({ size: textSize, weight }) =>
-    !weight ? `font-weight: ${fontWeights[textSize]};` : '';
+  paragraphTheme.extend = ({ size: textSize, weight }) => {
+    if (!weight) return `font-weight: ${fontWeights[textSize]};`;
+    if (weight === 'bold') return `font-weight: 500;`;
+    return '';
+  };
 
   const buttonKindTheme = {};
   buttonKinds.forEach((kind) => {
@@ -1707,7 +1713,7 @@ const buildTheme = (tokens, flags) => {
           },
         },
       },
-      extend: () => '',
+      extend: ({ weight }) => (weight === 'bold' ? 'font-weight: 500;' : ''),
     },
     icon: {
       disableScaleDown: true,
