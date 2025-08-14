c6d7c37407044c62dc0ab7f666d78bd0f1804814
Sulaymon333 <tjsulaymon@gmail.com>
2025-08-14 22:37:09 +0100

    Add video theme changes (#498)

--- src/js/themes/hpe.js
+++ src/js/themes/hpe.js
@@ -2550,6 +2550,16 @@ const buildTheme = (tokens, flags) => {
       },
       divider: false,
     },
+    video:{
+      scrubber: {
+        thickness: 'xsmall',
+      },
+      time: {
+        container: {
+          pad: { horizontal: 'xsmall'}
+        }
+      }
+    },
     // Theme-Designer only parameters
     name: 'HPE 1',
     rounding: 4,
