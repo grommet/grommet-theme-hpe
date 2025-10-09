8a5412bb66f4771cf4f7128ba8054c7dca3606f7
Jessica Rosenquist <54560994+jcfilben@users.noreply.github.com>
2025-10-09 11:29:45 -0600

    remove grommet-icons peerDependency

--- src/js/themes/hpe.js
+++ src/js/themes/hpe.js
@@ -10,9 +10,7 @@ import {
   global as localGlobal,
   components as localComponents,
 } from 'hpe-design-tokens/grommet';
-import { Hpe } from 'grommet-icons/icons/Hpe';
 
-// imports subject to change upon finalization of hpe-icons repo
 import { Checkmark } from '@hpe-design/icons-grommet/icons/Checkmark';
 import { Close } from '@hpe-design/icons-grommet/icons/Close';
 import { Filter } from '@hpe-design/icons-grommet/icons/Filter';
@@ -42,6 +40,28 @@ import { Up } from '@hpe-design/icons-grommet/icons/Up';
 
 import { backgrounds } from './backgrounds';
 import { colors } from './colors';
+
+function Hpe() {
+  return (
+    <svg
+      style={{
+        stroke: 'none',
+      }}
+      width="36"
+      height="18"
+      viewBox="0 0 48 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M2 6H46V18H2V6ZM5 9H43V15H5V9Z"
+        fill="#00C781"
+      />
+    </svg>
+  );
+}
 
 const baseSpacing = 24;
 
