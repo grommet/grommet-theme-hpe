a77555e0a27cf03099f3c4bb73905bf30cab99f9
Brittany <42451602+britt6612@users.noreply.github.com>
2026-03-19 11:45:22 -0600

    Fix pagination active state (#595)
    * fix the active border for pagination button
    
    * remove yalc changes
    
    * remove yalc changes
    
    * remove yalc changes

--- src/js/themes/hpe.js
+++ src/js/themes/hpe.js
@@ -2619,7 +2619,11 @@ const buildTheme = (tokens, flags) => {
         font: { weight: components.hpe.button.default.rest.fontWeight },
         active: {
           background: components.hpe.button.default.selected.rest.background,
-          border: { radius: components.hpe.button.default.medium.borderRadius },
+          border: {
+            radius: components.hpe.button.default.medium.borderRadius,
+            color: components.hpe.button.default.selected.rest.borderColor,
+            width: components.hpe.button.default.small.borderWidth,
+          },
           color: components.hpe.button.default.selected.rest.textColor,
           font: {
             weight: components.hpe.button.default.selected.rest.fontWeight,
