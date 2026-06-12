ae0fd802f093aeb0722f58af41670e0c1a43cb05
Matthew Glissmann <mdglissmann@gmail.com>
2026-06-12 12:02:35 -0600

    extract globals from orchestration

--- src/js/themes/hpe.js
+++ src/js/themes/hpe.js
@@ -59,9 +59,10 @@ import { buildFormTheme } from './form';
 import { buildLayoutTheme } from './layout';
 import { buildDeprecations } from './deprecations';
 import { getGraphikFamily, getGraphikFontFaces } from './fonts';
+import { buildGlobalTheme } from './global';
 
 const buildTheme = (tokens, flags) => {
-  const { light, dark, large, global, components } = tokens;
+  const { light, dark, global, components } = tokens;
 
   const dimensions = buildDimensions(tokens, flags);
 
@@ -145,154 +146,15 @@ const buildTheme = (tokens, flags) => {
 
   return deepFreeze({
     defaultMode: 'light',
-    global: {
+    global: buildGlobalTheme(tokens, {
       backgrounds,
-      ...dimensions,
       colors,
-      control: {
-        border: {
-          radius:
-            components.hpe.formField.default.medium.input.container
-              .borderRadius,
-          color:
-            components.hpe.formField.default.input.container.rest.borderColor,
-        },
-        disabled: { opacity: 0.3 },
-      },
-      deprecated: {
-        backgrounds: deprecations.backgrounds,
-        button: {
-          kind: deprecations.buttonKinds,
-        },
-        colors: deprecations.colors,
-      },
-      input: {
-        font: {
-          height: 'inherit',
-          weight: components.hpe.formField.default.medium.value.fontWeight,
-        },
-        padding: {
-          horizontal:
-            components.hpe.formField.default.medium.input.container.paddingX,
-          vertical:
-            components.hpe.formField.default.medium.input.container.paddingY,
-        },
-        readOnly: {
-          background:
-            components.hpe.formField.default.input.container.readOnly.rest
-              .background,
-          border: {
-            color:
-              components.hpe.formField.default.input.container.readOnly.rest
-                .borderColor,
-          },
-        },
-        extend: ({ theme }) => `
-          color: ${getThemeColor(
-            components.hpe.formField.default.value.rest.textColor,
-            theme,
-          )};
-          &::-webkit-input-placeholder {
-          font-weight: ${
-            components.hpe.formField.default.medium.placeholder.fontWeight
-          };
-        }
-      
-        &::-moz-placeholder {
-          font-weight: ${
-            components.hpe.formField.default.medium.placeholder.fontWeight
-          };
-        }
-      
-        &:-ms-input-placeholder {
-          font-weight: ${
-            components.hpe.formField.default.medium.placeholder.fontWeight
-          };
-        }
-        `,
-      },
-      font: {
-        family: global.hpe.fontStack.primary,
-        face: getGraphikFontFaces(family),
-        size: large.hpe.text.medium.fontSize,
-        height: large.hpe.text.medium.lineHeight,
-      },
-      focus: {
-        border: undefined,
-        outline: {
-          color: global.hpe.focusIndicator.outline.color,
-          size: global.hpe.focusIndicator.outline.width,
-          offset: global.hpe.focusIndicator.outlineOffset,
-        },
-        shadow: {
-          color: 'focus-support',
-          size: '2px',
-          blur: '0px',
-        },
-        twoColor: true,
-        inset: {
-          border: undefined,
-          outline: {
-            color: global.hpe.focusIndicator.outline.color,
-            size: global.hpe.focusIndicator.outline.width,
-            offset: `-${global.hpe.focusIndicator.outline.width}`,
-          },
-          shadow: {
-            color: 'focus-support',
-            size: '4px',
-            blur: '0px',
-            inset: true,
-          },
-          twoColor: true,
-        },
-      },
-      active: { background: 'background-active', color: 'active-text' },
-      drop: {
-        background: components.hpe.drop.default.background,
-        border: {
-          radius:
-            dimensions.radius[components.hpe.drop.default.borderRadius] ||
-            components.hpe.drop.default.borderRadius,
-        },
-        margin: components.hpe.drop.default.margin,
-        intelligentMargin: true,
-        shadowSize: 'medium',
-        /* HPE Global Header/Footer Service a.k.a. HPE Common HFWS sets the header
-         * at a z-index of 101. This adjustment brings Drop in alignment with Layer
-         * which needs an elevated z-index to sit atop the Global header. */
-        zIndex: components.hpe.drop.default.zIndex,
-        extend: () => `
-          [class*=MaskedInput__ContainerBox] {
-            padding-block: ${components.hpe.select.default.medium.drop.paddingY};
-            padding-inline: ${components.hpe.select.default.medium.drop.paddingX};
-            gap: ${components.hpe.select.default.medium.drop.gapY};
-            display: flex;
-            flex-direction: column;
-          }
-        `,
-      },
-      elevation: {
-        // Elevation values were derived from this Figma file.
-        // https://www.figma.com/file/eZYR3dtWdb9U90QvJ7p3T9/HPE-Color-Styles?node-id=405%3A25
-        // Naming in Figma file is strong/default/weak vs. Grommet t-shirt sizing.
-        // As defined here, default is currently mapping to medium.
-        light: {
-          small: light.hpe.shadow.small,
-          medium: light.hpe.shadow.medium,
-          large: light.hpe.shadow.large,
-        },
-        dark: {
-          small: dark.hpe.shadow.small,
-          medium: dark.hpe.shadow.medium,
-          large: dark.hpe.shadow.large,
-        },
-      },
-      hover: { background: 'background-hover', color: 'text-default' },
-      selected: {
-        background: 'background-selected-primary-strong',
-        color: 'text-onSelectedPrimaryStrong',
-      },
-    },
+      dimensions,
+      deprecations,
+      getThemeColor,
+      fontFaceFamily: family,
+      getGraphikFontFaces,
+    }),
     accordion: navigationTheme.accordion,
     anchor: typography.anchor,
     avatar: contentTheme.avatar,
