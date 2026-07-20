abfb7ab9a8c0b93eea5c22d95fab408c37ec9a95
Matthew Glissmann <mdglissmann@gmail.com>
2026-07-20 14:43:05 -0600

    build prism colors from tokens

--- src/js/themes/index.js
+++ src/js/themes/index.js
@@ -4,4 +4,4 @@ export { hpe } from './hpe';
 export * from './hpePop';
 export * from './backgrounds';
 export { colors } from './colors';
-export * from './prism';
+export { prism } from './prism';
--- src/js/themes/prism.js
+++ src/js/themes/prism.js
@@ -1,115 +1,128 @@
 // Theme for code syntax highlighting
 // Exported theme object is consumed by ReactSyntaxHighlighter + PrismJS
 // https://github.com/react-syntax-highlighter/react-syntax-highlighter#prism
-import { primitives, dark, light } from 'hpe-design-tokens/grommet';
-import { colors as hpeColors } from './colors';
+import {
+  primitives as localPrimitives,
+  dark as localDark,
+  light as localLight,
+  components as localComponents,
+} from 'hpe-design-tokens/grommet';
+import { buildColors } from './colors';
 
-const colors = {
-  atrule: {
-    dark: primitives.hpe.base.color['fuschia-200'],
-    light: primitives.hpe.base.color['fuschia-700'],
-  },
-  'attr-name': {
-    dark: primitives.hpe.base.color['plum-100'],
-    light: primitives.hpe.base.color['plum-600'],
-  },
-  'attr-value': {
-    dark: primitives.hpe.base.color['fuschia-200'],
-    light: primitives.hpe.base.color['fuschia-700'],
-  },
-  background: { dark: 'black', light: hpeColors['background-contrast'].light },
-  boolean: {
-    dark: primitives.hpe.base.color['plum-100'],
-    light: primitives.hpe.base.color['plum-600'],
-  },
-  'class-name': {
-    dark: primitives.hpe.base.color['coral-200'],
-    light: primitives.hpe.base.color['coral-500'],
-  },
-  builtin: {
-    dark: primitives.hpe.base.color['plum-100'],
-    light: primitives.hpe.base.color['plum-600'],
-  },
-  char: {
-    dark: primitives.hpe.base.color['plum-100'],
-    light: primitives.hpe.base.color['plum-600'],
-  },
-  code: {
-    dark: dark.hpe.color.text.default,
-    light: light.hpe.color.text.default,
-  },
-  comment: {
-    dark: primitives.hpe.base.color['green-550'],
-    light: primitives.hpe.base.color['green-800'],
-  },
-  entity: {
-    dark: primitives.hpe.base.color['blue-200'],
-    light: primitives.hpe.base.color['blue-700'],
-  },
-  function: {
-    dark: primitives.hpe.base.color['coral-200'],
-    light: primitives.hpe.base.color['coral-500'],
-  },
-  important: {
-    dark: primitives.hpe.base.color['fuschia-200'],
-    light: primitives.hpe.base.color['fuschia-700'],
-  },
-  inserted: {
-    dark: primitives.hpe.base.color['plum-100'],
-    light: primitives.hpe.base.color['plum-600'],
-  },
-  keyword: {
-    dark: primitives.hpe.base.color['blue-200'],
-    light: primitives.hpe.base.color['blue-700'],
-  },
-  'maybe-class-name': {
-    dark: primitives.hpe.base.color['purple-200'],
-    light: primitives.hpe.base.color['purple-700'],
-  },
-  number: {
-    dark: primitives.hpe.base.color['plum-100'],
-    light: primitives.hpe.base.color['plum-600'],
-  },
-  operator: {
-    dark: primitives.hpe.base.color['fuschia-200'],
-    light: primitives.hpe.base.color['fuschia-700'],
-  },
-  regex: {
-    dark: primitives.hpe.base.color['fuschia-200'],
-    light: primitives.hpe.base.color['fuschia-700'],
-  },
-  selector: {
-    dark: primitives.hpe.base.color['plum-100'],
-    light: primitives.hpe.base.color['plum-600'],
-  },
-  string: {
-    dark: primitives.hpe.base.color['plum-100'],
-    light: primitives.hpe.base.color['plum-600'],
-  },
-  url: {
-    dark: primitives.hpe.base.color['blue-200'],
-    light: primitives.hpe.base.color['blue-700'],
-  },
-  variable: {
-    dark: primitives.hpe.base.color['blue-200'],
-    light: primitives.hpe.base.color['blue-700'],
-  },
-  '.language-css .token.string': {
-    dark: primitives.hpe.base.color['blue-200'],
-    light: primitives.hpe.base.color['blue-700'],
-  },
-  '.style .token.string': {
-    dark: primitives.hpe.base.color['blue-200'],
-    light: primitives.hpe.base.color['blue-700'],
-  },
+const createPrismColors = (tokens) => {
+  const { primitives, dark, light } = tokens;
+  const hpeColors = buildColors(tokens);
+
+  return {
+    atrule: {
+      dark: primitives.hpe.base.color['fuschia-200'],
+      light: primitives.hpe.base.color['fuschia-700'],
+    },
+    'attr-name': {
+      dark: primitives.hpe.base.color['plum-100'],
+      light: primitives.hpe.base.color['plum-600'],
+    },
+    'attr-value': {
+      dark: primitives.hpe.base.color['fuschia-200'],
+      light: primitives.hpe.base.color['fuschia-700'],
+    },
+    background: {
+      dark: 'black',
+      light: hpeColors['background-contrast'].light,
+    },
+    boolean: {
+      dark: primitives.hpe.base.color['plum-100'],
+      light: primitives.hpe.base.color['plum-600'],
+    },
+    'class-name': {
+      dark: primitives.hpe.base.color['coral-200'],
+      light: primitives.hpe.base.color['coral-500'],
+    },
+    builtin: {
+      dark: primitives.hpe.base.color['plum-100'],
+      light: primitives.hpe.base.color['plum-600'],
+    },
+    char: {
+      dark: primitives.hpe.base.color['plum-100'],
+      light: primitives.hpe.base.color['plum-600'],
+    },
+    code: {
+      dark: dark.hpe.color.text.default,
+      light: light.hpe.color.text.default,
+    },
+    comment: {
+      dark: primitives.hpe.base.color['green-550'],
+      light: primitives.hpe.base.color['green-800'],
+    },
+    entity: {
+      dark: primitives.hpe.base.color['blue-200'],
+      light: primitives.hpe.base.color['blue-700'],
+    },
+    function: {
+      dark: primitives.hpe.base.color['coral-200'],
+      light: primitives.hpe.base.color['coral-500'],
+    },
+    important: {
+      dark: primitives.hpe.base.color['fuschia-200'],
+      light: primitives.hpe.base.color['fuschia-700'],
+    },
+    inserted: {
+      dark: primitives.hpe.base.color['plum-100'],
+      light: primitives.hpe.base.color['plum-600'],
+    },
+    keyword: {
+      dark: primitives.hpe.base.color['blue-200'],
+      light: primitives.hpe.base.color['blue-700'],
+    },
+    'maybe-class-name': {
+      dark: primitives.hpe.base.color['purple-200'],
+      light: primitives.hpe.base.color['purple-700'],
+    },
+    number: {
+      dark: primitives.hpe.base.color['plum-100'],
+      light: primitives.hpe.base.color['plum-600'],
+    },
+    operator: {
+      dark: primitives.hpe.base.color['fuschia-200'],
+      light: primitives.hpe.base.color['fuschia-700'],
+    },
+    regex: {
+      dark: primitives.hpe.base.color['fuschia-200'],
+      light: primitives.hpe.base.color['fuschia-700'],
+    },
+    selector: {
+      dark: primitives.hpe.base.color['plum-100'],
+      light: primitives.hpe.base.color['plum-600'],
+    },
+    string: {
+      dark: primitives.hpe.base.color['plum-100'],
+      light: primitives.hpe.base.color['plum-600'],
+    },
+    url: {
+      dark: primitives.hpe.base.color['blue-200'],
+      light: primitives.hpe.base.color['blue-700'],
+    },
+    variable: {
+      dark: primitives.hpe.base.color['blue-200'],
+      light: primitives.hpe.base.color['blue-700'],
+    },
+    '.language-css .token.string': {
+      dark: primitives.hpe.base.color['blue-200'],
+      light: primitives.hpe.base.color['blue-700'],
+    },
+    '.style .token.string': {
+      dark: primitives.hpe.base.color['blue-200'],
+      light: primitives.hpe.base.color['blue-700'],
+    },
+  };
 };
 
 // PrismJs + ReactSyntaxHighlighter implementation references:
 // https://github.com/react-syntax-highlighter/react-syntax-highlighter/blob/master/src/styles/prism/a11y-dark.js
 // Prism selectors: https://github.com/PrismJS/prism/blob/master/themes/prism.css
-const themeMode = (mode) => ({
+const themeMode = (mode, prismColors) => ({
   'code[class*="language-"]': {
-    color: colors.code[mode],
+    color: prismColors.code[mode],
     background: 'none',
     fontFamily: "'Fira Mono', monospace",
     textAlign: 'left',
@@ -127,8 +140,8 @@ const themeMode = (mode) => ({
     hyphens: 'none',
   },
   'pre[class*="language-"]': {
-    color: colors.code[mode],
-    background: colors.background[mode],
+    color: prismColors.code[mode],
+    background: prismColors.background[mode],
     fontFamily: "'Fira Mono', monospace",
     textAlign: 'left',
     whiteSpace: 'pre',
@@ -149,38 +162,52 @@ const themeMode = (mode) => ({
     borderRadius: '0.3em',
   },
   ':not(pre) > code[class*="language-"]': {
-    background: colors.background[mode],
+    background: prismColors.background[mode],
     padding: '0.1em',
     borderRadius: '0.3em',
     whiteSpace: 'normal',
   },
-  'class-name': { color: colors['class-name'][mode] },
-  'maybe-class-name': { color: colors['maybe-class-name'][mode] },
-  comment: { color: colors.comment[mode] },
-  function: { color: colors.function[mode] },
-  operator: { color: colors.operator[mode] },
-  string: { color: colors.string[mode] },
-  boolean: { color: colors.boolean[mode] },
-  number: { color: colors.number[mode] },
-  keyword: { color: colors.keyword[mode] },
-  selector: { color: colors.selector[mode] },
-  'attr-name': { color: colors['attr-name'][mode] },
-  char: { color: colors.char[mode] },
-  builtin: { color: colors.builtin[mode] },
-  inserted: { color: colors.inserted[mode] },
-  entity: { color: colors.entity[mode], cursor: 'help' },
-  url: { color: colors.url[mode] },
+  'class-name': { color: prismColors['class-name'][mode] },
+  'maybe-class-name': { color: prismColors['maybe-class-name'][mode] },
+  comment: { color: prismColors.comment[mode] },
+  function: { color: prismColors.function[mode] },
+  operator: { color: prismColors.operator[mode] },
+  string: { color: prismColors.string[mode] },
+  boolean: { color: prismColors.boolean[mode] },
+  number: { color: prismColors.number[mode] },
+  keyword: { color: prismColors.keyword[mode] },
+  selector: { color: prismColors.selector[mode] },
+  'attr-name': { color: prismColors['attr-name'][mode] },
+  char: { color: prismColors.char[mode] },
+  builtin: { color: prismColors.builtin[mode] },
+  inserted: { color: prismColors.inserted[mode] },
+  entity: { color: prismColors.entity[mode], cursor: 'help' },
+  url: { color: prismColors.url[mode] },
   '.language-css .token.string': {
-    color: colors['.language-css .token.string'][mode],
+    color: prismColors['.language-css .token.string'][mode],
   },
-  '.style .token.string': { color: colors['.style .token.string'][mode] },
-  variable: { color: colors.variable[mode] },
-  atrule: { color: colors.atrule[mode] },
-  'attr-value': { color: colors['attr-value'][mode] },
-  regex: { color: colors.regex[mode] },
-  important: { color: colors.important[mode], fontWeight: 'bold' },
+  '.style .token.string': { color: prismColors['.style .token.string'][mode] },
+  variable: { color: prismColors.variable[mode] },
+  atrule: { color: prismColors.atrule[mode] },
+  'attr-value': { color: prismColors['attr-value'][mode] },
+  regex: { color: prismColors.regex[mode] },
+  important: { color: prismColors.important[mode], fontWeight: 'bold' },
   bold: { fontWeight: 'bold' },
   italic: { fontStyle: 'italic' },
 });
 
-export const prism = { dark: themeMode('dark'), light: themeMode('light') };
+export const buildPrism = (tokens) => {
+  const prismColors = createPrismColors(tokens);
+
+  return {
+    dark: themeMode('dark', prismColors),
+    light: themeMode('light', prismColors),
+  };
+};
+
+export const prism = buildPrism({
+  primitives: localPrimitives,
+  dark: localDark,
+  light: localLight,
+  components: localComponents,
+});
